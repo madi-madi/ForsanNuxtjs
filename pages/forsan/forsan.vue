d95043cd769778bb4e42a23055c85675ec1f584a
madi-madi <ibrahim.s.m.2010@gmail.com>
2019-03-08 23:09:47 +0200

    forsan Nuxtjs

---
+++ pages/forsan/forsan.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h3>Forsan from {{ name }}</h3>
+		<NLink to="/"> Home</NLink>
+	</div>
+</template>
+<script>
+	export default{
+		asyncData(){
+			return {
+				name:process.static? 'static':(process.server?'server':'client')
+			}
+		},
+		head:{
+
+		}
+	}
+</script>
